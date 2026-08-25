bf1fee8121fb1cc0e9ad03255d302918bf816657
Margesh01 <margeshpatil01@gmail.com>
2026-08-25 10:59:26 +0530

    Initial commit with Web API, K8s manifests, and gitignore

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+﻿# Stage 1: Build stage using .NET 10 SDK
+FROM mcr.microsoft.com/dotnet/sdk:10.0 AS build
+WORKDIR /src
+
+# Copy csproj and restore dependencies
+COPY ["MySampleApi.csproj", "./"]
+RUN dotnet restore "MySampleApi.csproj"
+
+# Copy all files and publish
+COPY . .
+RUN dotnet publish "MySampleApi.csproj" -c Release -o /app/publish
+
+# Stage 2: Runtime stage using .NET 10 ASP.NET runtime
+FROM mcr.microsoft.com/dotnet/aspnet:10.0 AS final
+WORKDIR /app
+COPY --from=build /app/publish .
+
+EXPOSE 8080
+ENTRYPOINT ["dotnet", "MySampleApi.dll"]
---
+++ Program.cs
@@ -0,0 +1,26 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+// Maps the root URL (http://localhost:xxxx/) directly to weatherforecast
+// Maps the root URL directly to weatherforecast and returns the Pod Name
+app.MapGet("/", () => Results.Ok(new
+{
+    Message = "Hello from Kubernetes!",
+    ServedByPod = Environment.MachineName
+}));
+
+app.Run();
